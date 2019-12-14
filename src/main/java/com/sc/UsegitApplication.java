30016e99feb6690b452bf171cdc3345cb905e45e
sunchao <jo.sun.courage@hotmail.com>
2019-12-14 19:58:52 +0800

    测试idea的git功能

---
+++ src/main/java/com/sc/UsegitApplication.java
@@ -0,0 +1,13 @@
+package com.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsegitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsegitApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/sc/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.sc.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description TODO
+ * @Author sunchao
+ * @email jo.sun.courage@hotmail.com
+ * @Version 1.0
+ **/
+
+@RestController
+
+public class HelloController {
+
+    @RequestMapping("info")
+    public String info() {
+        return "Hello, SpringBoot";
+    }
+}
